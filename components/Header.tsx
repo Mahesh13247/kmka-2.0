67a276d1334b657a980068ace8636d7e9b96b99e
Mahesh13247 <139057471+Mahesh13247@users.noreply.github.com>
2025-11-12 15:06:15 +0530

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -63,7 +63,7 @@ export const Header: React.FC<HeaderProps> = ({ onSearch, onToggleFavoritesView,
                 {/* Default View */}
                 <h1 className="text-xl md:text-2xl font-bold text-white whitespace-nowrap">
                     <span className="bg-gradient-to-r from-white to-gray-400 text-transparent bg-clip-text">
-                        🥵
+                        Study Matrial
                     </span>
                 </h1>
                 
