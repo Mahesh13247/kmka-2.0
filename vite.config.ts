9619e0316eaaa0b09da7370110ced8698af052b2
Mahesh13247 <139057471+Mahesh13247@users.noreply.github.com>
2025-11-29 16:31:12 +0530

    Revert "Update vite.config.ts"
    This reverts commit 021e508cd59a814e77c058dc85729f1dddc3f2f8.

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig(({ mode }) => {
       host: '0.0.0.0',
       port: 4173,
       allowedHosts: [
-        'kmka.onrender.com',
+        'kmka-2-0-1.onrender.com',
         '.onrender.com'
       ]
     },
