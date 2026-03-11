b970cf9e6126d744c9513bab9ddf712e81631a68
Mahesh13247 <139057471+Mahesh13247@users.noreply.github.com>
2026-03-11 16:24:15 +0530

    update

--- index.tsx
+++ index.tsx
@@ -11,24 +11,24 @@ const rootElement = document.getElementById("root");
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
-// document.addEventListener("contextmenu", (event) => event.preventDefault());
+document.addEventListener("contextmenu", (event) => event.preventDefault());
 
-// const isCtrlShiftKey = (event: KeyboardEvent, key: string) =>
-//   event.ctrlKey && event.shiftKey && event.key.toUpperCase() === key.toUpperCase();
+const isCtrlShiftKey = (event: KeyboardEvent, key: string) =>
+  event.ctrlKey && event.shiftKey && event.key.toUpperCase() === key.toUpperCase();
 
-// document.addEventListener("keydown", (event) => {
-//   // Disable F12, Ctrl + Shift + I/J/C, Ctrl + U
-//   const isDisallowed =
-//     event.key === "F12" ||
-//     isCtrlShiftKey(event, "I") ||
-//     isCtrlShiftKey(event, "J") ||
-//     isCtrlShiftKey(event, "C") ||
-//     (event.ctrlKey && event.key.toUpperCase() === "U");
+document.addEventListener("keydown", (event) => {
+  // Disable F12, Ctrl + Shift + I/J/C, Ctrl + U
+  const isDisallowed =
+    event.key === "F12" ||
+    isCtrlShiftKey(event, "I") ||
+    isCtrlShiftKey(event, "J") ||
+    isCtrlShiftKey(event, "C") ||
+    (event.ctrlKey && event.key.toUpperCase() === "U");
 
-//   if (isDisallowed) {
-//     event.preventDefault();
-//   }
-// });
+  if (isDisallowed) {
+    event.preventDefault();
+  }
+});
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
